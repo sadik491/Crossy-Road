5adda10f8ea353008953006ee81d28cae80e73ed
Md Sadiqul Islam Khan <46827272+sadik491@users.noreply.github.com>
2020-03-12 18:26:26 +0600

    final complete

---
+++ Crossy Road/Assets/CarMove.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using PathCreation;
+
+public class CarMove : MonoBehaviour
+{
+
+    public PathCreator pathCreator;
+    public float speed = 5;
+    public float distanceTravel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        distanceTravel += speed * Time.deltaTime;
+        transform.position = pathCreator.path.GetPointAtDistance(distanceTravel);
+    }
+}
